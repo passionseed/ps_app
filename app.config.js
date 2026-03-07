4b2615f6940ca6edcdc21d403b7d1cec5b66c350
Bunyasit Fang <wachaa1319@gmail.com>
2026-03-07 15:19:44 -0800

    good

---
+++ app.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  expo: {
+    owner: "passionseed",
+    name: `Passion Seed${process.env.APP_NAME_SUFFIX || ""}`,
+    scheme: "passion-seed",
+    slug: "passion-seed",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      usesAppleSignIn: true,
+      icon: "./assets/pseed-app.icon",
+      bundleIdentifier: "com.passionseed.app",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/passionseed-logo-1024.png",
+        monochromeImage: "./assets/passionseed-logo-monochrome.png",
+        backgroundColor: "#ffffff",
+      },
+      icon: "./assets/passionseed-logo-1024.png",
+      predictiveBackGestureEnabled: false,
+      permissions: [
+        "android.permission.RECORD_AUDIO",
+        "android.permission.MODIFY_AUDIO_SETTINGS",
+        "android.permission.FOREGROUND_SERVICE",
+        "android.permission.FOREGROUND_SERVICE_MEDIA_PLAYBACK",
+      ],
+    },
+    web: {
+      bundler: "metro",
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      [
+        "expo-font",
+        {
+          fonts: [
+            "node_modules/@expo-google-fonts/google-sans/400Regular/GoogleSans_400Regular.ttf",
+            "node_modules/@expo-google-fonts/google-sans/700Bold/GoogleSans_700Bold.ttf",
+            "node_modules/@expo-google-fonts/libre-franklin/400Regular/LibreFranklin_400Regular.ttf",
+            "node_modules/@expo-google-fonts/libre-franklin/700Bold/LibreFranklin_700Bold.ttf",
+          ],
+        },
+      ],
+      "expo-router",
+      "expo-sqlite",
+      "expo-apple-authentication",
+      [
+        "expo-web-browser",
+        {
+          experimentalLauncherActivity: false,
+        },
+      ],
+      "expo-audio",
+      "expo-image",
+      "expo-sharing",
+      "expo-asset",
+    ],
+    extra: {
+      router: {},
+      eas: {
+        projectId: "baca732a-c7d8-4bd2-9742-48cc2a5e939f",
+      },
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/baca732a-c7d8-4bd2-9742-48cc2a5e939f",
+    },
+  },
+};
